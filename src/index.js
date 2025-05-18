0f174a936cd6b1bbbb9f366315a0ebb4414806d4
= <=>
2025-05-19 01:05:54 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(fileUpload({ useTempFiles: true, tempFileDir: '/tmp/' }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:5174', // Replace with your frontend URL
+  origin: ['http://localhost:5173','http://localhost:5174',process.env.FRONTEND_URL], // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
